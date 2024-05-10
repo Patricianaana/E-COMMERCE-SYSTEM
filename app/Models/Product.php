d506816ed3d53f53a7d27cd8fae1a70fcd48e20f
Tricia Kim <triciapany6@gmail.com>
2024-05-10 15:58:11 +0000

    setting product and category model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable=
+    [
+        'product_name',
+        'product_price',
+        'product_description',
+        'product_category',
+        'category_id',
+        'product_image'
+    ];
 }
