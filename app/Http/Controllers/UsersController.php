147c659b801bac9621ac22c2ac4f9ba4b55c20ba
Tricia Kim <triciapany6@gmail.com>
2024-05-10 13:29:06 +0000

    setting controller for add product function

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,5 +7,19 @@ use Illuminate\Http\Request;
 class UsersController extends Controller
 {
     //
-    
+    public function addProduct(Request $request)
+    {
+        $request->validate([
+            'product_name' => ['required', 'exists:wallets,id'],
+            'product_price' => ['required'],
+            'product_descrption'=>['required'],
+            'product_category'=>['required'],
+            'product_image'=>['required']
+        ]);
+
+    }
+
+
+
+
 }
